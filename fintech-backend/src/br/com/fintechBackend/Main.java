5a3a37ae4fc5253117662c25086e3e48c53d95f3
Guilherme Araujo <gui.araujo1902@gmail.com>
2024-03-08 19:49:17 -0300

    Gerando menu principal

--- fintech-backend/src/br/com/fintechBackend/Main.java
+++ fintech-backend/src/br/com/fintechBackend/Main.java
@@ -1,10 +1,39 @@
 package br.com.fintechBackend;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner scan = new Scanner(System.in);
 
+		System.out.println("*********************");
+		System.out.println("     FINTECH APP     ");
+		System.out.println("[1] - Criar conta    ");
+		System.out.println("[2] - Encerrar conta ");
+		System.out.println("[0] - Sair           ");
+		System.out.println("*********************");
+		System.out.println("Escolha: ");
+		int opcaoPrincipal = scan.nextInt();
+		
+		switch (opcaoPrincipal) {
+		case 1:
+			System.out.println("Criar");
+			break;
+		case 2:
+			System.out.println("Encerrar");
+			break;
+		case 0:
+			System.out.println("Sair");
+			break;
+		default:
+			System.out.println("Opção Inválida!");
+		}
+		
+		
+		
+		scan.close();
 	}
 
 }
